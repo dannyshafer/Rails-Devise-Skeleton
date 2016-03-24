fe3e67a4fc7e37defcd5bad0d4a5267424ec547e
Daniel Shafer <github@dannyshafer.com>
2016-03-24 09:28:18 -0700

    add devise controllers for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+      }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
