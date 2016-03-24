6cc40020e9993a8d9dfc6d0310c5e5853dc4fc34
Daniel Shafer <github@dannyshafer.com>
2016-03-24 09:32:05 -0700

    add devise controllers for users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,9 +8,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
